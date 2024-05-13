f9640e2c42157001ca537feed75a258d38c9f322
Jagriti Sharma <142136189+jagritiisharma@users.noreply.github.com>
2024-05-13 14:03:46 +0530

    first commit

---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+    {
+        id: "01",
+        name: "Dr. A.K Chaudhari",
+        specialization: "Surgeon",
+        avgRating: 4.8,
+        totalRating: 272,
+        photo: doctorImg01,
+        totalPatients: 1500,
+        hospital: "Mount Adora Hospital, Sylhet.",
+    },
+    {
+        id: "02",
+        name: "Dr. B.R Ambedkar",
+        specialization: "Neurologist",
+        avgRating: 4.8,
+        totalRating: 272,
+        photo: doctorImg02,
+        totalPatients: 1500,
+        hospital: "Mount Adora Hospital, Sylhet.",
+    },
+    {
+        id: "03",
+        name: "Dr. Raj sharma",
+        specialization: "Dermatologist",
+        avgRating: 4.8,
+        totalRating: 272,
+        photo: doctorImg03,
+        totalPatients: 1500,
+        hospital: "Mount Adora Hospital, Sylhet.",
+    },
+];
